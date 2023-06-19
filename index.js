709ad0050bfaeba83810f28e81ba8e7ed490087a
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2023-06-19 14:55:00 -0800

    comment this out (for now?) since it breaks passing more than one arg

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Sentry = require('@sentry/node');
 const lodash = require('lodash');
 
 const { app } = expressWs(express());
-require('merida').init();
+//require('merida').init();
 
 Sentry.init({
     dsn: process.env.DSN
